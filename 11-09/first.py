509d322a956d83ed301da63f109683fa0856a58d
Asteraceaeaa <kovinv454@gmail.com>
2024-09-12 23:11:01 +0500

    Complete first task from 11-09

---
+++ 11-09/first.py
@@ -0,0 +1,22 @@
+nums = list(map(int, input("Введите два числа через  - ").split(" ")))
+
+if nums[0] < nums[1]:
+    print(nums[0])
+else:
+    print(nums[1])
+
+
+
+#или просто
+# print(min(nums))
+
+
+
+
+
+
+
+
+
+
+
