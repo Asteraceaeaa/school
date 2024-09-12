909a565f217e8a50edeb5261d142fcb23fbcd23b
Asteraceaeaa <kovinv454@gmail.com>
2024-09-12 23:24:06 +0500

    Complete second task

---
+++ 11-09/second.py
@@ -0,0 +1,23 @@
+num = int(input("Введите целое число - "))
+
+if 1 <= num // 1000 <= 9:
+    print("YES")
+else:
+    print("NO")
+
+#или
+# if len(str(num)) == 4:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+
+
+
+
+
+
+
+
+
