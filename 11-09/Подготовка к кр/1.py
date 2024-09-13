54a72f07518018fa2f1e648f0a3224ad043f9cfd
Asteraceaeaa <kovinv454@gmail.com>
2024-09-13 08:08:29 +0500

    finish the first task in 'Подготовка к кр'

---
+++ 11-09/Подготовка к кр/1.py
@@ -0,0 +1,19 @@
+y = int(input("Enter the year "))
+
+if ((y // 1000) + (y // 100 % 10) + (y // 10 % 10) + (y % 10)) == 4:
+    print("Osobenniy")
+else:
+    print("not osobenniy")
+
+
+
+
+
+
+
+
+
+
+
+
+
