f2a14303291108e97f67d63e41b24c275c90b3ed
Asteraceaeaa <kovinv454@gmail.com>
2024-09-18 06:35:41 +0500

    9.py is finished

---
+++ 17-09/9.py
@@ -0,0 +1,19 @@
+stipendia = 22000; rashodi = 24000
+# initial_sum = mounths * (rashodi - stipendia)
+rashodi_with_inflation = 24000; base_inflation = 1.03
+mounths = 12
+
+first = 0; secnd = 0
+
+for mounth in range(1, mounths + 1):
+    first += rashodi - stipendia
+    secnd += rashodi_with_inflation - stipendia
+    print(rashodi_with_inflation)
+    print((mounth + 1) % 2 == 1)
+    if (mounth + 1) % 2 == 1: #С учетом того, что в первый месяц повышение цен отстуствует
+        rashodi_with_inflation *= base_inflation
+    else:
+        rashodi_with_inflation *= base_inflation + .02
+print(f"3.1: {first} \n3.2: {secnd}")
+
+
