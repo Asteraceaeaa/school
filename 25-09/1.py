a80b038aab7345590b6020a46516d89dca5ab7e3
asteraceaeaa <kovinv454@gmail.com>
2024-09-26 01:45:37 +0500

    start first task

---
+++ 25-09/1.py
@@ -0,0 +1,21 @@
+cd .
+
+
+
+git
+�ыясняет длину числа
+def length(n):
+    prev = 0
+    for i in range(2, 1000):
+        this = n % 10 ** (i + 1)
+        if prev == this:
+            length = i 
+            break
+        else:
+            prev = this
+
+    return length
+
+for i in range(100, n + 1):
+    for j in range(2, length(i) + 1):
+        print(j)
