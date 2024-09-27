029f5621794c4d87906cc0f5e90ebd969effc3e2
asteraceaeaa <kovinv454@gmail.com>
2024-09-27 21:49:50 +0500

    Finish the 5th task

---
+++ 25-09/5.py
@@ -0,0 +1,8 @@
+n = int(input("Enter n: "))
+summ = 0
+product = 1
+for i in range(1, n + 1):
+    summ += i
+    product *= i
+    
+print(f"The product of numbers for 1 to {n} = {product}, the sum of numbers = {summ}")
