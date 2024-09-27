e3f574e5c7e252995b72bbaec042e7398b87baf9
asteraceaeaa <kovinv454@gmail.com>
2024-09-28 02:53:41 +0500

    Finish the 9th task

---
+++ 25-09/9.py
@@ -0,0 +1,15 @@
+money = 10000; sale = 1
+cost = int(input("Enter rhe initial cost => "))
+sale_increase = int(input("Enter the sale increment => "))
+cnt = 0
+for i in range(money ** 10):
+    print(i)
+    if (money - cost) >= 0:
+        money -= cost
+        
+    else:
+        cnt = i
+        break
+    cost = cost // sale - (sale_increase / 100)
+print(f"Money remains: {round(money, 2)}, count of goods: {cnt}")
+    
